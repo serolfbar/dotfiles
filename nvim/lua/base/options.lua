b47c8a7bde2b423d89a1b3d689b93a5458806d3f
Alexander Barcenes <alexanderbarcenes@gmail.com>
2025-11-14 17:24:53 -0500

    Update to nvim setup

--- nvim/lua/base/options.lua
+++ nvim/lua/base/options.lua
@@ -6,19 +6,14 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.cmd("set nomodeline")
 -- DISABLING THIS FOR LSP_LINES to work
-vim.diagnostic.config({
-	virtual_lines = { current_line = true },
-	virtual_text = false
-})
-
-vim.cmd.colorscheme("minispring")
-require("mini.hues").setup({
-	accent = "bg",
-	background = "#17280e",
-	foreground = "#c4c8c2",
-	n_hues = 8,
-	plugins = {
-		default = true
-	},
-	saturation = "medium"
-})
+vim.cmd.colorscheme("kanso-mist")
+-- require("mini.hues").setup({
+-- 	accent = "bg",
+-- 	background = "#17280e",
+-- 	foreground = "#c4c8c2",
+-- 	n_hues = 8,
+-- 	plugins = {
+-- 		default = true
+-- 	},
+-- 	saturation = "medium"
+-- })
--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,8 +1,4 @@
 return {
-	{"rebelot/kanagawa.nvim"},
-	{"shaunsingh/solarized.nvim"},
-	{"sainnhe/everforest"},
-	{"miikanissi/modus-themes.nvim"},
-	{"projekt0n/github-nvim-theme"},
-	{"Mofiqul/vscode.nvim"}
+	{ "rktjmp/lush.nvim" },
+	{"webhooked/kanso.nvim"}
 }
